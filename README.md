6022497a0aa45a3ba394965aa8a42d683e57ee15
xxnavixx <xbf109k1x@gmail.com>
2019-04-05 18:05:16 +0900

    added radio buttons

--- README.md
+++ README.md
@@ -4,5 +4,4 @@ p5 rubik cube
 I don't know how to get current 3d matrix in p5.
 so i made my own transform matrix class.
 
-do not mix p5 tranform functions with these code.
-it will result unexpected transformation.
+mixing with p5 tranform functions may cause unexpected transformation.
--- main2.js
+++ main2.js
@@ -1,14 +1,7 @@
-let showtoggle = true;
-let stopToggle = false;
+let showtoggle = true, stopToggle = false;
 
 let img;
-let size = 3;
-
-let m1,m2,m3;
-let myTran;
-let textA;
 let rc;
-let test;
 
 function preload() {
 	img = loadImage('./image/1024px-Rainbow-gradient-fully-saturated-diagonal.svg.png');
@@ -16,8 +9,6 @@ function preload() {
 
 function setup() {
 	createCanvas(600,400,WEBGL);
-	
-	textA = createP();
 	stopBtn = createButton('stop');
 	stopBtn.mouseClicked(()=>{
 		if(stopToggle) loop(); else noLoop();
@@ -26,8 +17,45 @@ function setup() {
 	});
 	xBtn = createButton('toggle');
 	xBtn.mouseClicked(()=>{showtoggle = !showtoggle;});
+	
+	//--------------------------------------------
+	//--------------change these------------------
+	//--------------------------------------------
+	
+	let column = 4;
+	let singleCubeLength = 20; // px
+	let gap = 2; // px
+	
+	//--------------------------------------------
+	//--------------------------------------------
+	//--------------------------------------------
+	
+	rc = new RubikCube(column,singleCubeLength,gap);
+	let textC = createP('-- drag to change perspective');
+	let textA = createP('rotation axis');
+	let radioXYZ = createRadio();
+	radioXYZ.option('x');
+	radioXYZ.option('y');
+	radioXYZ.option('z');
+	radioXYZ.value('z');
+	
+	let textB = createP('column index');
+	let columnIndex = createRadio();
+	for(let i=0;i<rc.size;i++)
+		columnIndex.option(''+i);
+	columnIndex.value(''+(rc.size-1));
 		
-	rc = new RubikCube(1,30,2); // column, single cube length, gap
+	let rotationLeft = createButton('L');
+	let rotationRight = createButton('R');
+	rotationLeft.mouseClicked(()=>{
+		rc.rotateL(radioXYZ.value(),Number(columnIndex.value()));
+		// rc.rotateL();
+	});
+	
+	rotationRight.mouseClicked(()=>{
+		rc.rotateR(radioXYZ.value(),Number(columnIndex.value()));
+		// rc.rotateL();
+	});
 }
 
 function draw() {
